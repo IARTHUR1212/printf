b7364cae1600956aeb8db01ab8f47dc7428aa7b8
IARTHUR1212 <greatike2010@yahoo.com>
2023-08-22 16:27:56 -0700

    update

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-alex@ubuntu:~/c/printf$ cat main.c 
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
@@ -36,8 +35,8 @@ int main(void)
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
- 	len2 = printf("Percent:[%%]\n");
-    	_printf("Len:[%d]\n", len);
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
